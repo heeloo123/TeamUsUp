2470fba745f812325de48eace33d8de1d398327f
qian <qiantongk@gmail.com>
2023-02-22 20:59:35 +0800

    update alert

--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
+import Vue from'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
 
-
-//alert package
-import Vue from 'vue'
-import VueSweetalert2 from 'vue-sweetalert2';
-
-
-Vue.use(VueSweetalert2);
-
-new Vue({
-  el: '#app',
-  render: h => h(App)
-});
+//import sweetalert
+import VueSweetaleart2 from 'vue-sweetalert2';
+Vue.use(VueSweetaleart2);
