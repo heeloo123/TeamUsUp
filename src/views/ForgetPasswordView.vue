b0c41d6a78a191f517dddd209a8c3560c576e037
Ray0-0 <rachelhang13@gmail.com>
2023-02-26 18:06:40 +0800

    commit

--- src/views/ForgetPasswordView.vue
+++ src/views/ForgetPasswordView.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="container">
         <h1 class style="font-size:50px;">FORGET YOUR PASSWORD</h1>
+            <p>
+                To recover your account, please enter your email<br>address.
+            </p>
+                <p>
+                    The system will send a recovery link to your email<br>shortly. You can click on the link to recover your<br>account.
+                </p>
 
     </div>
 </template>
