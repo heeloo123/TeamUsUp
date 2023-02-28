81e0565587c6d1661d9df5b771da644cc6d711ab
qian <qiantongk@gmail.com>
2023-02-28 16:01:30 +0800

    change

--- src/views/CreateProjectView.vue
+++ src/views/CreateProjectView.vue
@@ -1,9 +1,9 @@
 <template>
   <form class="project-container">
-    <div style="margin:10px">
-      <h1 style="font-size: 50px;
-    font-family: math;
-    margin: 40px">Create A Project!</h1>
+    <div style="margin: 10px">
+      <h1 style="font-size: 50px; font-family: math; margin: 40px">
+        Create A Project!
+      </h1>
 
       <div class="project-pic">
         <input type="file" accept="image/*" @change="handleFileUpload" />
@@ -11,56 +11,64 @@
       <div style="margin-left: 400px; margin-top: -320px">
         <div class="insertP_name">
           <lable> Project Name: </lable>
-          <input v-model="projectName" type="text" placeholder="Enter project name.." required />
+          <input
+            v-model="projectName"
+            type="text"
+            placeholder="Enter project name.."
+            required
+          />
 
-          <div>
-            Project description :
-          </div>
-            <textarea v-model="projectDescription" required></textarea>
-         
-          </div>
-          <div class="searchU">
-            <label>Search for User:</label>
-            <input
-              v-model="searchQuery"
-              type="text"
-              placeholder="Search users..."
-            />
+          <div>Project description :</div>
+          <textarea v-model="projectDescription" required></textarea>
+        </div>
+        <div class="searchU">
+          <input v-model="search" type="text" placeholder="Search users..." />
+          <ul>
+            <li v-for="(user, index) in userList" :key="index">
+              {{ user }}
+              <button @click="deleteUser(index)">Delete</button>
+            </li>
+          </ul>
 
-            <!-- <ul>
-        <li v-for="user in filteredUsers" :key="user.id">
-          {{ user.name }}
-          <button @click="addMember(user)">Add</button>
-        </li>
-      </ul>
-      
-      <h3>Members</h3>
-      <ul>
-        <li v-for="member in members" :key="member.id">{{ member.name }}</li>
-      </ul>-->
-
-            <nav>
-              <button
-                class="defaultBtn"
-                type="submit"
-                @click.prevent="createProject"
-              >
-                Create Project
-              </button>
-              <button class="defaultBtn" type="submit">
-                <router-link to="/Profile">Cancel</router-link>
-              </button>
-            </nav>
-          </div>
-        
+          <nav>
+            <button
+              class="defaultBtn"
+              type="submit"
+              @click.prevent="createProject"
+            >
+              Create Project
+            </button>
+            <button class="defaultBtn" type="submit">
+              <router-link to="/Profile">Cancel</router-link>
+            </button>
+          </nav>
+        </div>
       </div>
     </div>
   </form>
 </template>
 
+<!---need import axios-for search-->
 <script>
 export default {
   name: "CreateProjectView",
+  data() {
+    return {
+      search: '',
+      userList: ['Jane','John'] 
+    }
+  },
+  methods: {
+    addUser() {
+      if (this.search && !this.userList.includes(this.search)) {
+        this.userList.push(this.search)
+        this.search = ''
+      }
+    },
+    deleteUser(index) {
+      this.userList.splice(index, 1)
+    }
+  }
 };
 </script>
 
@@ -79,7 +87,7 @@ export default {
   background: rgb(234, 231, 231);
   overflow: hidden;
   align-items: center;
-  border-radius:20px;
+  border-radius: 20px;
 }
 
 input[type="file"] {
@@ -89,28 +97,26 @@ input[type="file"] {
   margin: 60px;
 }
 
-.insertP_name{
-
-margin: 20px;
-font-size:40px;
-font-family:math;
-padding: 10px;
-
+.insertP_name {
+  margin: 20px;
+  font-size: 40px;
+  font-family: math;
+  padding: 10px;
 }
-.insertP_name input{
-font-size:20px;
-min-width: 700px;
-width:auto;
-height: 50px;
-margin: 10px;
-padding-left: 20px;
-border-radius: 10px;
+.insertP_name input {
+  font-size: 20px;
+  min-width: 700px;
+  width: auto;
+  height: 50px;
+  margin: 10px;
+  padding-left: 20px;
+  border-radius: 10px;
 }
 
-.insertP_name textarea{
-width: 1200px;
-min-height:130px;
-height: auto;
-border-radius:30px
+.insertP_name textarea {
+  width: 1200px;
+  min-height: 130px;
+  height: auto;
+  border-radius: 30px;
 }
 </style>
