5a47475ab411dc4ff7591b70b3444944279e6023
qian <qiantongk@gmail.com>
2023-02-28 16:57:52 +0800

    page view

--- src/views/CreateProjectView.vue
+++ src/views/CreateProjectView.vue
@@ -8,7 +8,26 @@
       <div class="project-pic">
         <input type="file" accept="image/*" @change="handleFileUpload" />
       </div>
-      <div style="margin-left: 400px; margin-top: -320px">
+      <div class="searchU">
+        <input v-model="search" type="text" placeholder="Search users..." />
+         <ul>
+          <li v-for="(user, index) in userList" :key="index">
+            {{ user }}
+            <button
+              style="background: transparent; border: transparent"
+              @click="deleteUser(index)"
+            >
+              <img
+                style="width: 20px; height: 20px"
+                src="../assets/delete.png"
+                alt="delete icon"
+              />
+            </button>
+          </li>
+        </ul>
+     </div>
+
+      <div style="margin-left: 400px; margin-top: -720px">
         <div class="insertP_name">
           <lable> Project Name: </lable>
           <input
@@ -20,30 +39,26 @@
 
           <div>Project description :</div>
           <textarea v-model="projectDescription" required></textarea>
-        </div>
-        <div class="searchU">
-          <input v-model="search" type="text" placeholder="Search users..." />
-          <ul>
-            <li v-for="(user, index) in userList" :key="index">
-              {{ user }}
-              <button @click="deleteUser(index)">Delete</button>
-            </li>
-          </ul>
+</div></div>
 
           <nav>
-            <button
-              class="defaultBtn"
-              type="submit"
-              @click.prevent="createProject"
+            <label>
+              <button class="defaultBtn" type="submit">
+                <router-link to="/Profile">Cancel</router-link>
+              </button></label
             >
-              Create Project
-            </button>
-            <button class="defaultBtn" type="submit">
-              <router-link to="/Profile">Cancel</router-link>
-            </button>
+            <label style="margin-left: 1200px"
+              ><button
+                class="defaultBtn"
+                type="submit"
+                @click.prevent="createProject"
+              >
+                Create Project
+              </button></label
+            >
           </nav>
-        </div>
-      </div>
+        
+      
     </div>
   </form>
 </template>
@@ -54,21 +69,21 @@ export default {
   name: "CreateProjectView",
   data() {
     return {
-      search: '',
-      userList: ['Jane','John'] 
-    }
+      search: "",
+      userList: ["Jane", "John"],
+    };
   },
   methods: {
     addUser() {
       if (this.search && !this.userList.includes(this.search)) {
-        this.userList.push(this.search)
-        this.search = ''
+        this.userList.push(this.search);
+        this.search = "";
       }
     },
     deleteUser(index) {
-      this.userList.splice(index, 1)
-    }
-  }
+      this.userList.splice(index, 1);
+    },
+  },
 };
 </script>
 
@@ -81,8 +96,8 @@ export default {
 }
 
 .project-pic {
-  width: 300px;
-  height: 300px;
+  width: 250px;
+  height: 250px;
   margin: 40px;
   background: rgb(234, 231, 231);
   overflow: hidden;
@@ -115,8 +130,32 @@ input[type="file"] {
 
 .insertP_name textarea {
   width: 1200px;
-  min-height: 130px;
+  min-height: 400px;
   height: auto;
   border-radius: 30px;
+}
+.searchU input {
+  margin-left: 20px;
+  padding: 15px;
+  min-width: 300px;
+  width: auto;
+  border-radius: 20px;
+  font-size:15px;
+}
+
+.searchU ul {
+  background: rgb(239, 232, 232);
+  display: content;
+  width: 300px;
+  min-height: 200px;
+  height: auto;
+  margin-left: 20px;
+  border-radius: 20px;
+  padding: 20px;
+}
+.searchU li {
+  font-size: 30px;
+  margin-left: 40px;
+  padding: 10px;
 }
 </style>
