1c8c3bbfb085a7f61617aa4cbfa246a6026f8924
Ray0-0 <rachelhang13@gmail.com>
2023-03-01 20:56:20 +0800

    commit

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,22 +1,26 @@
 <template>
-  <h1>Admin page</h1>
-    <div class="container">
-      <div v-for="profile in profiles" :key="profile.name">
+  <div style="display: flex; text-align: -webkit-center">
+    <div class="background">
+      <h1>Admin page</h1>
+        <div class="container">
+          <div v-for="profile in profiles" :key="profile.name">
 
-        <div class="profile">
-            <p>Student Name: {{ profile.firstName }} {{ profile.lastName }}<br>
-            Major: {{ profile.major }}<br>
-            Account Status: {{ profile.accountStatus }}<br></p>
-        </div>
-      </div>
+            <div class="profile">
+              <p>Student Name: {{ profile.firstName }} {{ profile.lastName }}<br>
+              Major: {{ profile.major }}<br>
+              Account Status: {{ profile.accountStatus }}<br></p>
+            </div>
+          </div>
 
-        <div class="defaultBtn">
-          <button>Unlock Account</button>
+            <div class="defaultBtn">
+              <button>Unlock Account</button>
+            </div>
+            <div class="defaultBtn">
+              <button>Archive</button>
+            </div>
         </div>
-        <div class="defaultBtn">
-          <button>Archive</button>
-        </div>
-    </div>
+     </div>
+  </div>
 </template>
 
 <script>
@@ -39,9 +43,14 @@ export default {
  
 };
 
-
 </script>
 
 <style>
+.container{
+  display: flex;
+  margin: 50px;
+  background: rgb(211, 208, 208);
+  border-radius: 10px;
+}
 
 </style>
