818fc563b3f89928e6c91473b3d9e71b58d5ef31
qian <qiantongk@gmail.com>
2023-03-01 12:14:59 +0800

    add constranit

--- src/views/CreateProjectView.vue
+++ src/views/CreateProjectView.vue
@@ -1,27 +1,21 @@
 <template>
   <form class="project-container">
     <div style="margin: 10px">
-      <h1 style="font-size: 50px; font-family: math; margin: 40px">
-        Create A Project!
-      </h1>
+      <h1 style="font-size: 50px; font-family: math; margin: 40px">Create A Project!</h1>
 
       <div class="project-pic">
         <input type="file" accept="image/*" @change="handleFileUpload" />
       </div>
       <div class="searchU">
-        <input v-model="search" type="text" placeholder="Search users..." />
+        <input v-model="search" type="text" placeholder="Search users..." @keydown.enter="addUser" />
+        <button class="addBtn" @click="addUser" v-if="search && !userList.includes(search)">
+          Add
+        </button>
         <ul>
           <li v-for="(user, index) in userList" :key="index">
             {{ user }}
-            <button
-              style="background: transparent; border: transparent"
-              @click="deleteUser(index)"
-            >
-              <img
-                style="width: 20px; height: 20px"
-                src="../assets/delete.png"
-                alt="delete icon"
-              />
+            <button style="background: transparent; border: transparent" @click="deleteUser(index)">
+              <img style="width: 20px; height: 20px" src="../assets/delete.png" alt="delete icon" />
             </button>
           </li>
         </ul>
@@ -30,46 +24,39 @@
       <div style="margin-left: 400px; margin-top: -670px">
         <div class="insertP_name">
           <lable> Project Name: </lable>
-          <input
-            v-model="projectName"
-            type="text"
-            placeholder="Enter project name.."
-            required
-          />
+          <input v-model="projectName" type="text" placeholder="Enter project name.." required />
 
           <div>Project description :</div>
           <textarea v-model="projectDescription" required></textarea>
         </div>
       </div>
-
-      <nav>
-        <label>
-          <button class="defaultBtn" type="submit">
-            <router-link to="/Profile">Cancel</router-link>
-          </button></label
-        >
-        <label style="margin-left: 1200px"
-          ><button
-            class="defaultBtn"
-            type="submit"
-            @click.prevent="createProject"
-          >
-            Create Project
-          </button></label
-        >
-      </nav>
+      <div style="margin-top: 150px">
+        <nav>
+          <label style="float: left">
+            <button class="defaultBtn" type="submit">
+              <router-link to="/Profile">Cancel</router-link>
+            </button></label>
+          <label style="float: right"><button class="defaultBtn" type="submit" @click.prevent="createProject">
+              Create Project
+            </button></label>
+        </nav>
+      </div>
     </div>
   </form>
 </template>
 
-<!---need import axios-for search-->
+<!---need import axios-for search input @hadelserach input-->
 <script>
+const Swal = require("sweetalert2");
 export default {
   name: "CreateProjectView",
   data() {
     return {
       search: "",
-      userList: ["Jane", "John"],
+      userList: ["John", "Jane", "jackson"],
+      projectName: "",
+      projectDescription: "",
+      projectPic: null,
     };
   },
   methods: {
@@ -81,6 +68,33 @@ export default {
     },
     deleteUser(index) {
       this.userList.splice(index, 1);
+    },
+    createProject() {
+      const invalidinput = /[~`!#$%^&*|\\:<>]/; // regular expression pattern
+      if (
+        this.projectName &&
+        !invalidinput.test(this.projectName) &&
+        this.projectName.trim() !== "" &&
+        this.projectDescription &&
+        !invalidinput.test(this.projectDescription) &&
+        this.projectDescription.trim() !== "" 
+        
+      ) {
+        Swal.fire({
+          icon: "success",
+
+          text: "Project has been created!",
+        });
+
+        // Navigate to projectpage
+        this.$router.push({ path: "/Project" });
+      } else {
+        Swal.fire({
+          icon: "warning",
+
+          text: "The text should not include~`!#$%^&*|\\:<>",
+        });
+      }
     },
   },
 };
@@ -117,6 +131,7 @@ input[type="file"] {
   font-family: math;
   padding: 10px;
 }
+
 .insertP_name input {
   font-size: 20px;
   min-width: 700px;
@@ -132,7 +147,10 @@ input[type="file"] {
   min-height: 400px;
   height: auto;
   border-radius: 30px;
+  padding: 20px;
+  font-size: 20px;
 }
+
 .searchU input {
   margin-left: 20px;
   padding: 15px;
@@ -152,14 +170,17 @@ input[type="file"] {
   border-radius: 20px;
   padding: 20px;
 }
+
 .searchU li {
   font-size: 30px;
   margin-left: 40px;
   padding: 10px;
 }
 
-.defaultBtn{
-  position: relative;
-  top: 70px;
+.addBtn {
+  margin: 10px;
+  border-radius: 30px;
+  color: white;
+  background: black;
 }
 </style>
