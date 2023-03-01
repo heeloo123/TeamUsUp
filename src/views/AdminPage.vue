e8c5ccbb2122a56b206d000e7c3d5daec7bbf5cc
Ray0-0 <rachelhang13@gmail.com>
2023-03-01 20:05:45 +0800

    commit

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -2,19 +2,19 @@
   <h1>Admin page</h1>
     <div class="container">
         <div> 
-          Student Name :
+          Student Name:
             <div class="Student Name">
               {{ profile.firstName }} {{ profile.lastname }}
             </div>
         </div>
         <div>
-          Major :
+          Major:
             <div class="major">
               {{ profile.major }}
             </div>
         </div>
         <div>
-          Account Status :
+          Account Status:
           <div class="accountStatus">
             {{ profile.accountStatus }}
           </div>
