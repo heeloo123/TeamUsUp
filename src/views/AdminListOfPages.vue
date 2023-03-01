cacb1a0c5b8c5aaf2d2446916941a76763cc30a2
Ray0-0 <rachelhang13@gmail.com>
2023-03-01 22:59:51 +0800

    commit

---
+++ src/views/AdminListOfPages.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li>
+            <button type="button">Previous</button>
+        </li>
+        <li>
+            <button type="button">Next</button>
+        </li>
+    </ul>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
