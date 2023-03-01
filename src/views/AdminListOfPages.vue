30f381c86a7c9c45d0d764208eeebe454941a6dc
Ray0-0 <rachelhang13@gmail.com>
2023-03-01 23:19:18 +0800

    commit

--- src/views/AdminListOfPages.vue
+++ src/views/AdminListOfPages.vue
@@ -27,8 +27,40 @@
         currentPage: {
             type: Number,
             required: true,
+        },
+
+        totalPages: {
+            type: Number,
+            required: true,
+        },
+    },
+
+    computed: {
+        startPage() {
+        if (this.currentPage === 1) {
+            return 1;
         }
+
+        return this.currentPage -1;
+    },
+    pages() {
+        const range = [];
+        for (
+            let x = this.startPage;
+            x <= Math.min (this.startPage + this.maxButtons -1, this.totalPages);
+            x ++
+        )
+            {
+                range.push(
+                    {
+                        name: x,
+                        isDisabled: x === this.currentPage
+                    }
+                )
+            }
     }
+        
+        
 };
 
 </script>
