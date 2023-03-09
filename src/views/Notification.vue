c2de716ea93ccc7a087c1e68a4d47a9d6095ea02
Ray0-0 <rachelhang13@gmail.com>
2023-03-09 18:46:57 +0800

    commit

--- src/views/Notification.vue
+++ src/views/Notification.vue
@@ -77,7 +77,6 @@ export default {
             formatDate(date) {
                const options = {year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'}
                return new Date(date).toLocaleDateString('en-gb', options);
-                
             },
 
 
