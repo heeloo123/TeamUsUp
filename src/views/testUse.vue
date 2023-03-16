b078c4002195a6d067a86121392130aa20fcb5b4
qian <qiantongk@gmail.com>
2023-03-16 12:14:31 +0800

    teset

--- src/views/testUse.vue
+++ src/views/testUse.vue
@@ -1,23 +1,20 @@
 <template>
- 
- <div>{{ notifications }}  </div> 
+  <div>{{ notifications }}</div>
 
-
-
-
-<h2>Notifications</h2>
-    <ul>
-      <li v-for="(notification, index) in notifications" :key="index">
-        <div :class="{ 'read': notification.read, 'unread': !notification.read }">
-          <span>{{ notification.message }}</span>
-          <button v-if="!notification.read" @click="markAsRead(notification)">Mark as Read</button>
-        </div>
-      </li>
-    </ul>
+  <h2>Notifications</h2>
+  <ul>
+    <li v-for="(notification, index) in notifications" :key="index">
+      <div :class="{ read: notification.read, unread: !notification.read }">
+        {{ notification.notificationId }} <span>{{ notification.message }}</span>
+        <button v-if="!notification.read" @click="markAsRead(notification.notificationId)">
+          Mark as Read
+        </button>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
-
 import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
 //import Swal from "sweetalert2";
@@ -25,20 +22,18 @@ import { useAuthStore } from "@/stores/auth";
 const API_URL = "http://49.245.48.28:8080/api";
 
 export default {
-  name: "ProfileView",
-  props: ["profileID"],
+  name: "testPage",
+ 
   data() {
     return {
       notifications: [],
-     New_notifications:[],
+      New_notifications: [],
     };
   },
 
   async mounted() {
     const auth = useAuthStore();
     if (auth.isAuthenticated) {
-     
-
       const headers = {
         "session-ID": auth.jsessionID,
       };
@@ -47,23 +42,46 @@ export default {
         .get(`${API_URL}/notification/retrieveAny?email=${auth.email}`, { headers })
         .then((response) => {
           console.log(response.data);
-          this.message = response.data;
+          this.notifications = response.data;
         })
         .catch((error) => {
           console.error(error);
-        })
-        axios
+        });
+      axios
         .get(`${API_URL}/notification/retrieveNew?email=${auth.email}`, { headers })
         .then((response) => {
           console.log(response.data);
-          this.Nmessage = response.data;
+          this.New_notifications = response.data;
         })
         .catch((error) => {
           console.error(error);
-        })
-
+        });
     }
-  }
-}
-</script>
+  },
+  methods: {
+    async markAsRead(notificationId) {
+      try {
+        const auth = useAuthStore();
+        if (auth.isAuthenticated) {
+          axios
+            .post(
+              `${API_URL}/notification/markAsRead?notificationID=${notificationId}`,
+              {},
 
+              {
+                headers: {
+                  "session-ID": auth.jsessionID != null ? auth.jsessionID : "Placeholder",
+                },
+              }
+            )
+            .then((response) => {
+              console.log(response);
+            });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
