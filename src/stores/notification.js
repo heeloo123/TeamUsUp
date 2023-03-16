31a65f5fd08eb30651971f28f2a5d629b12974c3
ReinaSparklechu <Sparklechus@gmail.com>
2023-03-16 11:21:00 +0800

    Notification retrieval for new and existing complete

--- src/stores/notification.js
+++ src/stores/notification.js
@@ -1,57 +1,57 @@
 import { defineStore } from 'pinia';
+import {useAuthStore} from "@/stores/auth";
 import axios from 'axios';
-const API_URL ="http://49.245.48.28:8080/api";
+const API_URL ="http://49.245.48.28:8080/api/notification";
 
 export const useNotificationStore = defineStore ({
     id: "notification",
     state: () => ({
-        notification: {}
+        notifications: []
     }),
 
     actions: {
-        async fetchNewNotification(email) {
+        async fetchNewNotification() {
+            const auth = useAuthStore()
             try {
             const response = await axios.get(`${API_URL}/retrieveNew`, {
-                params: {
-                    email,
+                headers: {
+                    'session-ID':auth.jsessionID
                 }
             });
-            this.notifications= response.data.map(notification => notification.message);
+            this.notifications = response.data;
         }
             catch(error) {
                 console.error(error);
             }
-        }
-    },
-
-    async fetchAnyNotification(email) {
-        try {
-            const response = await axios.get(`${API_URL}/retrieveAny`, {
-                params: {
-                    email,
-                }
-            });
-            this.notifications = response.data.map(notification => notification.message);
-        }
-          catch(error) {
-            console.error(error);
-          }
+        },
+        async fetchAnyNotification() {
+            const auth = useAuthStore()
+            try {
+                const response = await axios.get(`${API_URL}/retrieveAny`, {
+                    headers: {
+                        'session-ID':auth.jsessionID
+                    }
+                });
+                this.notifications = response.data.map(notification => notification.message);
+            }
+            catch(error) {
+                console.error(error);
+            }
         },
 
         async markAsRead(notification, notificationID) {
             if (!notification.read) {
-            try {
-                await axios.post(`${API_URL}/markAsRead/${notificationID}`, {readFlag: true});
-                notification.read = true;
-                this.$emit('read');
+                try {
+                    await axios.post(`${API_URL}/markAsRead/${notificationID}`, {readFlag: true});
+                    notification.read = true;
+                    this.$emit('read');
+                }
+                catch(error) {
+                    console.error(error);
+                }
             }
-              catch(error) {
-                console.error(error);
-              }    
-            }
-            
-        },
 
+        },
         async processAction(notificationID, action) {
             try {
                 await axios.post(`${API_URL}/processAction`, {
@@ -61,9 +61,14 @@ export const useNotificationStore = defineStore ({
                     }
                 })
             }
-              catch(error) {
+            catch(error) {
                 console.error(error);
-              }
+            }
         }
+    },
+
+
+
+
     }
 )
