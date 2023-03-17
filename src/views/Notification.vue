93e4a6d01ab026a483f8e09f420e21e8fd065b2c
ReinaSparklechu <Sparklechus@gmail.com>
2023-03-17 14:32:29 +0800

    Notification popup mechanic fixed-ish

--- src/views/Notification.vue
+++ src/views/Notification.vue
@@ -162,6 +162,11 @@ export default {
               text: action ? 'Successfully verified project participation':'Successfully declined project participation',
               icon: "success",
             }).then(() => this.$forceUpdate)
+          } else{
+            Swal.fire({
+              text:'Something went wrong, please try again later',
+              icon:'error'
+            })
           }
         })
         .catch((error) => {
